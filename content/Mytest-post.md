0e32a13b041abc24ef576f513d2e93a9a9cf6866
Mindbuild <ratiertm72@gmail.com>
2025-03-16 11:57:40 +0900

    lotechnica

---
+++ content/Mytest-post.md
@@ -0,0 +1,16 @@
+Title: MyNew Post
+Date: 2025-03-16
+Status: published
+Tags: pelican, postgres
+Jumbo_image: images/postgres-full-text-search-with-django/haystack_large.jpg
+Jumbo_color: dark
+Jumbo_attr: Pexels
+Jumbo_attr_link: https://www.pexels.com/photo/field-village-summer-agriculture-33079/
+Small_image: images/postgres-full-text-search-with-django/haystack_small.jpg
+
+
+
+22세기, 인류는 화성 너머로 나아가고 있습니다...
+이건 markdown 이나
+
+# pelican
---
+++ pelicanconf.py
@@ -0,0 +1,68 @@
+AUTHOR = 'DongBuem Lee'
+SITENAME = 'lchfkorea'
+SITESUBTITLE = "first trys by MindBuild"
+SITEURL = 'https://lchfkorea.com'
+
+PATH = 'content'
+STATIC_PATHS = ['images', 'static']
+STATIC_EXCLUDES = ['.sass-cache']
+AUTHOR_SAVE_AS = ''
+
+USE_FOLDER_AS_CATEGORY = True
+
+# Locale settings
+TIMEZONE = 'Asia/seoul'
+DEFAULT_LANG = 'ko'
+DEFAULT_DATE_FORMAT = '%B %-d, %Y'
+
+# Theme settings
+THEME = "theme"
+PYGMENTS_STYLE = "native"
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = True
+USE_OPEN_GRAPH = True
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Normal Technologies LLC', 'https://www.normal.tech/'),
+    ('Type-in Games', 'https://www.typeingames.com/'),
+    ('AnswerCast', 'https://www.typeingames.com/answercast/'),
+)
+
+# Social widget
+SOCIAL = (
+    ('GitHub', 'https://www.github.com/ratiertm'),
+)
+
+DEFAULT_PAGINATION = 40
+
+DEFAULT_METADATA = {
+    'status': "draft",
+}
+
+# Plugins
+# PLUGIN_PATHS = ['pelican-plugins']
+# PLUGINS = ["render_math"]
+# MARKDOWN = {
+#     'extension_configs': {
+#         'markdown.extensions.codehilite': {'css_class': 'highlight'},
+#         'markdown.extensions.extra': {},
+#         'markdown.extensions.meta': {},
+#     },
+#     'output_format': 'html5',
+# }
+#
+# # MathJax
+# MATH_JAX = {
+#     'responsive': True
+# }
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
